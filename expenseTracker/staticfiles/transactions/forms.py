22b1ae8edb9846f33ea7f8cbdeb12ea2eabfdea2
D-Curse <dhruvbhatada@gmail.com>
2023-12-14 14:44:36 +0530

    changes

--- expenseTracker/staticfiles/transactions/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Transaction
-
-class TransactionForm(forms.ModelForm):
-    class Meta:
-        model = Transaction
-        fields = ['name' ,'amount', 'category']
